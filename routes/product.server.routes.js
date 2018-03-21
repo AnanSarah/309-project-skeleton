c3d3e8f842e83c11cc9e95e724aca91b5bdde62f
Anan Sarah <anansarahnikita27@gmail.com>
2018-03-21 03:42:20 +0000

    Assignment 13 progress

--- routes/product.server.routes.js
+++ routes/product.server.routes.js
@@ -2,6 +2,10 @@
 
  var products = require('./../controllers/products.server.controller.js');
  var users = require('./../controllers/users.server.controller.js');
+     app.route('/api/products')
+	.get(products.list)
+	.post(users.requiresLogin, products.create);
+
 
   app.route('/product/new').get(products.createView);
 
